0196212436bd18d92463d5a4ab99f103a90a2d1f
janessa06 <145781730+Janessa90@users.noreply.github.com>
2026-02-20 14:01:11 +0800

    Delete harvestiq-main/app/models directory

--- harvestiq-main/app/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from app.models.user import User
-from app.models.product import Product
-from app.models.order import Order, OrderItem
-from app.models.review import Review
-from app.models.category import Category
-from app.models.message import Conversation, Message
