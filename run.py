4403a952afdc7c274d396fa1768e638ccf4be806
janessa06 <145781730+Janessa90@users.noreply.github.com>
2026-02-23 17:04:29 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,11 +1,10 @@
 from app import create_app, socketio
 
-<<<<<<< HEAD
 flask_app = create_app()
 
 if __name__ == "__main__":
     socketio.run(flask_app, host="0.0.0.0", port=5000, debug=True)
-=======
+
 app = create_app()
 
 print("APP VALUE:", app)
@@ -17,4 +16,3 @@ if __name__ == "__main__":
         port=5000,
         debug=False
     )
->>>>>>> 24211ea (Updated AI search, weighing system, admin approval, dashboards)
